9f3ce72cd5e18fb305beb2ce2cb0532160609908
Thomas <thomas.meyet@epitech.eu>
2024-11-03 15:36:26 +0100

    main

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import HomePage from './components/home/home.js';
+
+function App() {
+  return (
+      <Fragment>
+        <HomePage />
+      </Fragment>
+  );
+}
+
+export default App;
+
---
+++ portfolio/src/components/home/home.js
@@ -0,0 +1,47 @@
+import './Home.css';
+import Logo from '../../assets/logo.png';
+import React from 'react';
+
+function HomePage() {
+    const styles = {
+        textAlign: 'center',
+        fontFamily: 'Montserrat',
+        overflowX: 'hidden',
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        background: '#161647',
+        lineHeight: 1.7,
+        color: '#fff',
+        transform: 'skewY(-5deg)',
+      };
+    return (
+        <div style={styles }>
+            <div className="bg-text-container">
+                <div className="animate-text">
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                </div>
+                <div className="animate-text left">
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                    <span>Portfolio&nbsp;</span>
+                </div>
+            </div>
+            <div className="container">
+                <div className="col">
+                    <img src={Logo} alt="My logo" className="start-logo" />
+                    <h1>Thomas MEYET</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
